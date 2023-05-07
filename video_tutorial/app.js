209de4ee3db76854942f75c2fd0016b3420a3286
Karolina Redden <dubajkaro@gmail.com>
2023-05-07 17:53:26 -0300

    add the progress

---
+++ video_tutorial/app.js
@@ -0,0 +1,37 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const circulationRepo = require('./repos/circulationRepo')
+const data = require('./circulation.json');
+
+const dbName = 'circulation';
+
+const url = "mongodb+srv://karo:karo@cluster0.wf4cdac.mongodb.net/circulation"
+
+async function main() {
+  // try {
+  const client = new MongoClient(url, { useUnifiedTopology: true });
+  await client.connect();
+  try {
+    const results = await circulationRepo.loadData(data);
+    assert.equal(data.length, results.insertedCount);
+    const getData = await circulationRepo.get();
+    assert.equal(data.length, getData.length);
+    const filterData = await circulationRepo.get({ Newspaper: getData[4].Newspaper });
+    assert.equal(filterData[0], getData[4]);
+  } catch (error) {
+    console.log(error)
+  } finally {
+    const admin = client.db(dbName).admin();
+    await client.db(dbName).dropDatabase();
+    // console.log(await admin.serverStatus());
+    console.log(await admin.listDatabases());
+    client.close();
+  }
+}
+
+main();
+
+
+
+
+
---
+++ video_tutorial/repos/circulationRepo.js
@@ -0,0 +1,43 @@
+const { MongoClient } = require('mongodb');
+
+function circulationRepo() {
+  const url = "mongodb+srv://karo:karo@cluster0.wf4cdac.mongodb.net/circulation"
+  const dbName = 'circulation';
+
+  function get() {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url, { useUnifiedTopology: true });
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+        let items = db.collection('newspapers').find();
+        resolve(await items.toArray());
+        client.close();
+      } catch (error) {
+        reject(error)
+      }
+    })
+
+  }
+
+  function loadData(data) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url, { useUnifiedTopology: true });
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+        results = await db.collection('newspapers').insertMany(data);
+        resolve(results);
+        client.close();
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+  return { loadData, get }
+}
+
+module.exports = circulationRepo();
+
+
+//ask Andrew about data
