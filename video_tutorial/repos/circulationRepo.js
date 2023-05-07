0a778b42c2974237c16b00d4820ffbf1e34d41e4
Karolina Redden <dubajkaro@gmail.com>
2023-05-07 17:58:43 -0300

    add change, to test rebase and merge

--- video_tutorial/repos/circulationRepo.js
+++ video_tutorial/repos/circulationRepo.js
@@ -4,13 +4,13 @@ function circulationRepo() {
   const url = "mongodb+srv://karo:karo@cluster0.wf4cdac.mongodb.net/circulation"
   const dbName = 'circulation';
 
-  function get() {
+  function get(query) {
     return new Promise(async (resolve, reject) => {
       const client = new MongoClient(url, { useUnifiedTopology: true });
       try {
         await client.connect();
         const db = client.db(dbName);
-        let items = db.collection('newspapers').find();
+        let items = db.collection('newspapers').find(query);
         resolve(await items.toArray());
         client.close();
       } catch (error) {
@@ -40,4 +40,3 @@ function circulationRepo() {
 module.exports = circulationRepo();
 
 
-//ask Andrew about data
